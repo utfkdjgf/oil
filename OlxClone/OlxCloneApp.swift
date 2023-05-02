d2849145b17a738cf82d0e903037cfa764120da4
mikolaura <mikolaura58@gmail.com>
2023-05-02 18:28:05 +0300

    Initial Commit

---
+++ OlxClone/OlxCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  OlxCloneApp.swift
+//  OlxClone
+//
+//  Created by uran on 02.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct OlxCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
